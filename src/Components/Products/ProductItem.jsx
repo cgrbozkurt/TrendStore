8316db270f6ed1001bf9c1815ec942075ccc81de
cgrbozkurt <cagribozzkurt@gmail.com>
2023-09-13 23:57:36 +0300

    trendstore

---
+++ src/Components/Products/ProductItem.jsx
@@ -0,0 +1,15 @@
+
+// eslint-disable-next-line react/prop-types
+const ProductItem = ({product}) => {
+  const {name,description,img,price}=procut;
+    return (
+   
+   
+   <li>
+    <img src={img} alt="" />
+    
+    </li>
+  )
+}
+
+export default ProductItem
---
+++ src/Components/Products/Products.jsx
@@ -0,0 +1,17 @@
+import products from "../../ProductData";
+import ProductItem from "./ProductItem";
+import "./Products.css";
+
+const Products = () => {
+  const productList = products.map((product) => (
+    <ProductItem product={product} key={product.id} />
+  ));
+
+  return (
+    <main className="products-wrapper">
+      <ul className="products">{productList}</ul>
+    </main>
+  );
+};
+
+export default Products;
