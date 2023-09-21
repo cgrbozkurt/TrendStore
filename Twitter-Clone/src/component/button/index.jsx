dc569d3b2be211549ac0b2ff9cfb8afb10fd704b
cgrbozkurt <cagribozzkurt@gmail.com>
2023-09-21 23:39:33 +0300

    twitter

--- Twitter-Clone/src/component/button/index.jsx
+++ Twitter-Clone/src/component/button/index.jsx
@@ -1,17 +1,17 @@
 import classNames from "classnames";
 import { createElement } from "react";
+import PropTypes from "prop-types";
 
 
 
 export default function Button({size,children}){
-    return(
-        createElement('button',
-        {className=classNames(
-          {""}
-        )}
-        ))
+    return  createElement('button',
+        {className:classNames("bg-[1d9bf0] rounded-full",{
+          "px-4 ":size==='normal'
+        } )}
+        )
 }
 
 Button.propTypes={
-    size:PropTypes.oneOfType
+    size:PropTypes.oneOf(['normal'],['large'])
 }
