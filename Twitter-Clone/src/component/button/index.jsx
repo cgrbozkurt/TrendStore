344d1c9f00803ab418aa15d0e0a7becfedcaf345
cgrbozkurt <cagribozzkurt@gmail.com>
2023-09-23 22:09:07 +0300

    twclone

--- Twitter-Clone/src/component/button/index.jsx
+++ Twitter-Clone/src/component/button/index.jsx
@@ -7,10 +7,10 @@ export default function Button({ size, children }) {
     "button",
     {
       className: classNames(
-        "bg-[#1d9bf0] rounded-full flex item-center justify-center font-bold hover:bg-[#1a8cd8] transition-colors ",
+        "bg-[#1d9bf0] rounded-full flex items-center justify-center font-bold hover:bg-[#1a8cd8] transition-colors ",
         {
           "px-4 h-9 ": size === "normal",
-          "px-4 h-[52px]  w-full": size === "large",
+          "px-4 h-[52px] text-[17px] w-full": size === "large",
         }
       ),
     },
