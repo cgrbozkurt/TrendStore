0a4bf7717f0a33c2ca98d3f8ca72496ef08adeb8
cgrbozkurt <cagribozzkurt@gmail.com>
2023-09-28 14:52:51 +0300

    clone

--- Twitter-Clone/src/store/auth/index.js
+++ Twitter-Clone/src/store/auth/index.js
@@ -1,7 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState={
-currentAccount:false,
+currentAccount:   {
+    id:1,
+    username:"Bozkurt",
+    fullName:"Çağrı Bozkurt",
+    avatar:"https://twitter.com/arBozkurt7/photo"
+},
 accounts:[]
 }
 
@@ -11,8 +16,20 @@ const auth = createSlice({
     reducers:{
         //state Manipüle metodları 
     
-    _setAccounts:(state,action)=>{
-        
+    _addAccounts:(state,action)=>{
+        state.accounts.push(action.payload)
+    },
+    _removeAccounts:(state,action)=>{
+state.accounts=state.action.filter(account=>account.id !== action.payload)
+if( state.currentAccount&& action.payload===state.currentAccount.id){
+this._setCurrentAccount(false)
+}
+    },
+    _setCurrentAccount:(state,action)=>{
+        state.currentAccount=action.payload
     }
     }
 })
+
+export const {_addAccounts,_removeAccounts,_setCurrentAccount}=auth.actions;
+export default auth.reducer
