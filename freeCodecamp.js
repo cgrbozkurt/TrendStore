f112736601c6dea7f39b7d783555fe51681e38ac
cgrbozkurt <cagribozzkurt@gmail.com>
2023-10-03 23:44:37 +0300

    afsd

--- freeCodecamp.js
+++ freeCodecamp.js
@@ -1,21 +1,34 @@
 function randomFraction() {
-    // Only change code below this line.
+   
     let result = 0;
-    // Math.random() can generate 0. We don't want to     return a 0,
-    // so keep generating random numbers until we get one     that isn't 0
+    
     while (result === 0) {
       result = Math.random();
     }
   
     return result;
-    // Only change code above this line.
   }var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
 
   function randomWholeNum() {
-    // Only change code below this line.
+  
     return Math.floor(Math.random() * 10);
   }
 
   function randomRange(myMin, myMax) {
     return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
+  }
+
+  function convertToInteger(str) {
+    return parseInt(str);
+  }
+  
+  convertToInteger("56");
+
+  function convertToInteger(str) {
+    return parseInt(str, 2);
+  }
+  convertToInteger("10011");
+
+  function checkEqual(a, b) {
+    return a === b ? "Equal" : "Not Equal";
   }
