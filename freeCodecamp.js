47fddce1fec060f4a9a859cf4cbc2a9bbebcf127
cgrbozkurt <cagribozzkurt@gmail.com>
2023-10-04 23:18:08 +0300

    afsd

--- freeCodecamp.js
+++ freeCodecamp.js
@@ -32,3 +32,139 @@ function randomFraction() {
   function checkEqual(a, b) {
     return a === b ? "Equal" : "Not Equal";
   }
+function randomFraction() {
+   
+    let result = 0;
+    
+    while (result === 0) {
+      result = Math.random();
+    }
+  
+    return result;
+  }var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+  function randomWholeNum() {
+  
+    return Math.floor(Math.random() * 10);
+  }
+
+  function randomRange(myMin, myMax) {
+    return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
+  }
+
+  function convertToInteger(str) {
+    return parseInt(str);
+  }
+  
+  convertToInteger("56");
+
+  function convertToInteger(str) {
+    return parseInt(str, 2);
+  }
+  convertToInteger("10011");
+
+  function checkEqual(a, b) {
+    return a === b ? "Equal" : "Not Equal";
+  }
+function randomFraction() {
+   
+    let result = 0;
+    
+    while (result === 0) {
+      result = Math.random();
+    }
+  
+    return result;
+  }var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+  function randomWholeNum() {
+  
+    return Math.floor(Math.random() * 10);
+  }
+
+  function randomRange(myMin, myMax) {
+    return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
+  }
+
+  function convertToInteger(str) {
+    return parseInt(str);
+  }
+  
+  convertToInteger("56");
+
+  function convertToInteger(str) {
+    return parseInt(str, 2);
+  }
+  convertToInteger("10011");
+
+  function checkEqual(a, b) {
+    return a === b ? "Equal" : "Not Equal";
+  }
+function randomFraction() {
+   
+    let result = 0;
+    
+    while (result === 0) {
+      result = Math.random();
+    }
+  
+    return result;
+  }var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+  function randomWholeNum() {
+  
+    return Math.floor(Math.random() * 10);
+  }
+
+  function randomRange(myMin, myMax) {
+    return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
+  }
+
+  function convertToInteger(str) {
+    return parseInt(str);
+  }
+  
+  convertToInteger("56");
+
+  function convertToInteger(str) {
+    return parseInt(str, 2);
+  }
+  convertToInteger("10011");
+
+  function checkEqual(a, b) {
+    return a === b ? "Equal" : "Not Equal";
+  }
+function randomFraction() {
+   
+    let result = 0;
+    
+    while (result === 0) {
+      result = Math.random();
+    }
+  
+    return result;
+  }var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+  function randomWholeNum() {
+  
+    return Math.floor(Math.random() * 10);
+  }
+
+  function randomRange(myMin, myMax) {
+    return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
+  }
+
+  function convertToInteger(str) {
+    return parseInt(str);
+  }
+  
+  convertToInteger("56");
+
+  function convertToInteger(str) {
+    return parseInt(str, 2);
+  }
+  convertToInteger("10011");
+
+  function checkEqual(a, b) {
+    return a === b ? "Equal" : "Not Equal";
+  }
