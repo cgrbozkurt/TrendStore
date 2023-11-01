eb256841998919e03e8dda7753c13bd44495903b
cgrbozkurt <cagribozzkurt@gmail.com>
2023-11-01 10:35:59 +0300

    money

--- money/src/App.js
+++ money/src/App.js
@@ -7,12 +7,16 @@ import Product from "./Components/Product"
 function App() {
 
   const [money , setMoney]=useState(10000);
+  const [basket, setBasket]=useState([]);
+  useEffect(()=>{
+    console.log(basket);
+  },[basket]) 
 
   return (
   <>
    <Header money={money}  />
 {Products.map(product=>(
-  <Product key={product.id} product={product} />
+  <Product key={product.id} product={product}  basket={basket} setBasket={setBasket} />
 ))}
 
   </>
--- money/src/Components/Product.jsx
+++ money/src/Components/Product.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
-const Products = ({product}) => {
+const Products = ({product,basket,setBasket}) => {
+
+  const basketItem=basket.find(item=>item.id===product.id)
+
+  const addBasket=()=>{
+const checkBasket=basket.find(item=>item.id===product.id)
+if(checkBasket){
+checkBasket.amount+=1
+setBasket([...basket.filter(item=>item.id!==product.id),checkBasket])
+}
+else{
+  setBasket([...basket,{
+    id:product.id,
+amount:1
+  }])
+}
+  }
+
   return (
-<>
-<h1 className='text-red-600'>{product.title}</h1>
-</>
+
+<Fragment >
+<div className="flex-col p-3 mb-3 border-l-stone-950 ">
+<h6 className='pb-3 text-lg'>{product.title}</h6>
+<div>${product.price} </div>
+<div className="actions ">
+  <button className='border-solid border-2 rounded-lg w-auto'>Sat</button>
+  <span>{basketItem && basketItem.amount || 0}</span>
+  <button  onClick={addBasket} className='border-solid border-2 rounded-lg w-auto'>Satın Al</button>
+
+
+</div>
+</div>
+</Fragment>
     )
 }
 
